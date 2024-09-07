308440eada179dca5a40a3f610144f18748bd1d3
jihong <hongjoonie91@gmail.com>
2024-09-07 15:05:29 +0900

    호텔 상세페이지 / 등록 페이지 추가

--- js/hotel.js
+++ js/hotel.js
@@ -1,6 +1,6 @@
 $(function() {
-    //input을 datepicker로 선언
-    $('#datepicker1, #datepicker2').datepicker({
+    // 달력 아이콘 있는 데이터피커
+    $('.icon_datepicker').datepicker({
         dateFormat: 'yy-mm-dd' //달력 날짜 형태
         ,showOtherMonths: true //빈 공간에 현재월의 앞뒤월의 날짜를 표시
         ,showMonthAfterYear: true // 월- 년 순서가아닌 년도 - 월 순서
@@ -17,10 +17,26 @@ $(function() {
         ,dayNames: ['일요일','월요일','화요일','수요일','목요일','금요일','토요일'] //달력의 요일 Tooltip
         ,minDate: "-5Y" //최소 선택일자(-1D:하루전, -1M:한달전, -1Y:일년전)
         ,maxDate: "+5y" //최대 선택일자(+1D:하루후, -1M:한달후, -1Y:일년후)  
-    });                    
-   
+    });
+
+    // 아이콘 없는 데이터피커
+    $('.ipt_datepicker').datepicker({
+        dateFormat: 'yy-mm-dd' //달력 날짜 형태
+        ,showOtherMonths: true //빈 공간에 현재월의 앞뒤월의 날짜를 표시
+        ,showMonthAfterYear: true // 월- 년 순서가아닌 년도 - 월 순서
+        ,changeYear: true //option값 년 선택 가능
+        ,changeMonth: true //option값  월 선택 가능
+        ,yearSuffix: "년" //달력의 년도 부분 뒤 텍스트
+        ,monthNamesShort: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'] //달력의 월 부분 텍스트
+        ,monthNames: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'] //달력의 월 부분 Tooltip
+        ,dayNamesMin: ['일','월','화','수','목','금','토'] //달력의 요일 텍스트
+        ,dayNames: ['일요일','월요일','화요일','수요일','목요일','금요일','토요일'] //달력의 요일 Tooltip
+        ,minDate: "-5Y" //최소 선택일자(-1D:하루전, -1M:한달전, -1Y:일년전)
+        ,maxDate: "+5y" //최대 선택일자(+1D:하루후, -1M:한달후, -1Y:일년후)  
+    });
+
     //초기값을 오늘 날짜로 설정해줘야 합니다.
-    $('#datepicker1, #datepicker2').datepicker('setDate', 'today'); //(-1D:하루전, -1M:한달전, -1Y:일년전), (+1D:하루후, -1M:한달후, -1Y:일년후)            
+    $('.icon_datepicker, .ipt_datepicker').datepicker('setDate', 'today'); //(-1D:하루전, -1M:한달전, -1Y:일년전), (+1D:하루후, -1M:한달후, -1Y:일년후)            
 
     // 맞춤 추천 디테일 정보
     $('.detail_btn').click(function() {
@@ -31,5 +47,28 @@ $(function() {
         }
     });
 
+    // 객실당 인원 수
+    $('.ipt_detail_btn').click(function() {
+        if($('.ipt_detail').css('display') == 'none'){
+            $('.ipt_detail').show();
+        } else {
+            $('.ipt_detail').hide();
+        }
+    });
+
+    // tooltip
+    $('.tooltip_btn').click(function() {
+        if($('.tooltip_cont').css('display') == 'none'){
+            $('.tooltip_cont').show();
+        } else {
+            $('.tooltip_cont').hide();
+        }
+    });
+
+    $('.timeSelect').select2({
+        minimumResultsForSearch: Infinity
+    });
     
 });
+
+
